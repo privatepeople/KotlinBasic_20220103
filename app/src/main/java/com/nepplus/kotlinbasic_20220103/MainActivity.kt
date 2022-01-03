a2a7692224680e720c029c724375376f878e5548
Lee Seung Hun <negrat@naver.com>
2022-01-03 15:52:10 +0900

    xml태그 -> kt에서 끌어다 사용할 준비     - 코틀린 확장팩

--- app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
@@ -2,10 +2,12 @@ package com.nepplus.kotlinbasic_20220103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
