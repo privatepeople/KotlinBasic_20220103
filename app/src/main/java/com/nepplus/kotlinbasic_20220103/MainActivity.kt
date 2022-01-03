48532494baa960b0bf2874a530dcda60de090747
Lee Seung Hun <negrat@naver.com>
2022-01-03 16:11:34 +0900

    추가 UI 구성     - 입력한 내용을, 토스트로 띄우기

--- app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.kotlinbasic_20220103
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 //          로그 찍기 버튼 눌리면 할 일 세팅
-        btnLog.setOnClickListener { it: View!
-
+        btnLog.setOnClickListener {
 //                버튼 눌리면 할일
-            Log.d(tag:"메인화면", msg:"로그찍기 버튼 눌림")
+            Log.d("메인화면", "로그찍기 버튼 눌림")
+            Log.e("메인화면", "로그찍기 e로 로그")
+
+        }
+
+//          토스트 띄우기 버튼 눌리면 할 일
+        btnToast.setOnClickListener {
+
+//              토스트로 "안녕하세요." 문장 띄우기
+            Toast.makeText(this, "안녕하세요", Toast.LENGTH_SHORT).show()
 
         }
 
