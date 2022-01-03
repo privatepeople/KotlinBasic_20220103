d56d17bdd03a638746ceb6b80bd6e73b95fa6b50
Lee Seung Hun <negrat@naver.com>
2022-01-03 15:56:43 +0900

    xml태그 -> kt에서 끌어다 사용할 준비     - 코틀린 확장팩

--- app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
@@ -2,12 +2,21 @@ package com.nepplus.kotlinbasic_20220103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//          로그 찍기 버튼 눌리면 할 일 세팅
+        btnLog.setOnClickListener { it: View!
+
+//                버튼 눌리면 할일
+            Log.d(tag:"메인화면", msg:"로그찍기 버튼 눌림")
+
+        }
 
     }
 }
